686708f511bf42189a79084e8af193a523cbc7a6
Yarre <yaroslav.syvukha@gmail.com>
2026-02-19 22:22:19 +0100

    feat: base middleware and interceptor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TransformInterceptor } from './transform.interceptor';
 
 @Controller()
+@UseInterceptors(TransformInterceptor)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const PORT = process.env.PORT ?? 3000;
+const SWAGGER_ROUTE = 'api';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder().build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(SWAGGER_ROUTE, app, documentFactory);
+
+  await app.listen(PORT);
+
+  console.log(`Backend is running on: http://localhost:${PORT}`);
+  console.log(
+    `Swagger is running on: http://localhost:${PORT}/${SWAGGER_ROUTE}`,
+  );
 }
-bootstrap();
+
+void bootstrap();

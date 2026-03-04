bb02039781918edb1a5ee89d6eaaa6f7a54ee27d
Yarre <yaroslav.syvukha@gmail.com>
2026-03-04 15:31:21 +0100

    feat: base auth\users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggingMiddleware } from './logging.middleware';
 import { PrismaService } from './services/prisma.client';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })

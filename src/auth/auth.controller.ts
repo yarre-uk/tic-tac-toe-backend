af6d4e98bb7df3ca6f8a2f5f07998bf225ad7ade
Yarre <yaroslav.syvukha@gmail.com>
2026-03-07 17:44:58 +0100

    feat: do logout and refresh auth controller methods

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,17 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto as SignInDto } from './dtos/sign-in.dto';
 import { SignUpDto as SignUpDto } from './dtos/sign-up.dto';
-import type { Response } from 'express';
+import type { Request, Response } from 'express';
 import { ConfigService } from '@nestjs/config';
+import { isDefined } from '@/utils';
 
 export const REFRESH_TOKEN_KEY = 'refreshToken';
 
@@ -14,6 +22,16 @@ export class AuthController {
     private readonly configService: ConfigService,
   ) {}
 
+  private _setRefreshTokenCookie(response: Response, refreshToken: string) {
+    response.cookie(REFRESH_TOKEN_KEY, refreshToken, {
+      maxAge:
+        +this.configService.getOrThrow<number>('REFRESH_TOKEN_TTL') * 1000,
+      secure: true,
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+  }
+
   @Post('sign-in')
   async signIn(
     @Res({ passthrough: true }) response: Response,
@@ -21,11 +39,7 @@ export class AuthController {
   ) {
     const { accessToken, refreshToken } = await this.authService.signIn(dto);
 
-    response.cookie(REFRESH_TOKEN_KEY, refreshToken, {
-      maxAge:
-        +this.configService.getOrThrow<number>('REFRESH_TOKEN_TTL') * 1000,
-      secure: true,
-    });
+    this._setRefreshTokenCookie(response, refreshToken);
 
     return { accessToken };
   }
@@ -37,20 +51,49 @@ export class AuthController {
   ) {
     const { accessToken, refreshToken } = await this.authService.signUp(dto);
 
-    response.cookie(REFRESH_TOKEN_KEY, refreshToken, {
-      maxAge:
-        +this.configService.getOrThrow<number>('REFRESH_TOKEN_TTL') * 1000,
-      secure: true,
-    });
+    this._setRefreshTokenCookie(response, refreshToken);
 
     return { accessToken };
   }
 
   @Post('logout')
-  logOut() {
-    //TODO get actual token from cookies
-    const refreshToken = '';
+  async logOut(
+    @Res({ passthrough: true }) response: Response,
+    @Req() request: Request,
+  ) {
+    const refreshToken = request.cookies[REFRESH_TOKEN_KEY] as
+      | string
+      | undefined;
 
-    return this.authService.logOut(refreshToken);
+    if (!isDefined(refreshToken)) {
+      throw new UnauthorizedException('No token provided!');
+    }
+
+    await this.authService.logOut(refreshToken);
+
+    response.clearCookie(REFRESH_TOKEN_KEY);
+
+    return { message: 'Logged out successfully' };
+  }
+
+  @Post('refresh')
+  async refresh(
+    @Res({ passthrough: true }) response: Response,
+    @Req() request: Request,
+  ) {
+    const refreshToken = request.cookies[REFRESH_TOKEN_KEY] as
+      | string
+      | undefined;
+
+    if (!isDefined(refreshToken)) {
+      throw new UnauthorizedException('No refresh token provided');
+    }
+
+    const { accessToken, refreshToken: newRefreshToken } =
+      await this.authService.refresh(refreshToken);
+
+    this._setRefreshTokenCookie(response, newRefreshToken);
+
+    return { accessToken };
   }
 }

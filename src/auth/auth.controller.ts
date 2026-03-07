02166dfaa676acf6ba08aa099b51c1fd98791d09
Yarre <yaroslav.syvukha@gmail.com>
2026-03-07 10:28:15 +0100

    feat: set refreshToken to cookies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,26 +1,47 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto as SignInDto } from './dtos/sign-in.dto';
 import { SignUpDto as SignUpDto } from './dtos/sign-up.dto';
+import type { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+
+export const REFRESH_TOKEN_KEY = 'refreshToken';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post('sign-in')
-  async signIn(@Body() dto: SignInDto) {
+  async signIn(
+    @Res({ passthrough: true }) response: Response,
+    @Body() dto: SignInDto,
+  ) {
     const { accessToken, refreshToken } = await this.authService.signIn(dto);
 
-    console.log(refreshToken);
+    response.cookie(REFRESH_TOKEN_KEY, refreshToken, {
+      maxAge:
+        +this.configService.getOrThrow<number>('REFRESH_TOKEN_TTL') * 1000,
+      secure: true,
+    });
 
     return { accessToken };
   }
 
   @Post('sign-up')
-  async signUp(@Body() dto: SignUpDto) {
+  async signUp(
+    @Res({ passthrough: true }) response: Response,
+    @Body() dto: SignUpDto,
+  ) {
     const { accessToken, refreshToken } = await this.authService.signUp(dto);
 
-    console.log(refreshToken);
+    response.cookie(REFRESH_TOKEN_KEY, refreshToken, {
+      maxAge:
+        +this.configService.getOrThrow<number>('REFRESH_TOKEN_TTL') * 1000,
+      secure: true,
+    });
 
     return { accessToken };
   }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
 
 const PORT = process.env.PORT ?? 3000;
 const SWAGGER_ROUTE = 'api';
@@ -9,6 +10,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder().build();
+
+  app.use(cookieParser());
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(SWAGGER_ROUTE, app, documentFactory);
